ea50253231148a2516486951355d422d889389a7
Eduardo Carreiro <eduardo@wpcwebsolutions.com>
2022-04-24 18:17:46 -0300

    route cursos done

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-const route = require("./routes");
+const routes = require("./routes");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(route);
+app.use(routes);
 
 // definicao de endereço e url
-const hostname = "127.0.0.1";
+const hostname = "localhost";
 const port = 4000;
 app.listen(port, () => {
 	console.log(`Server running at http://${hostname}:${port}/`);
